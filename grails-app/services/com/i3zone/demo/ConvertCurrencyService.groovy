937f67bfb820aee8c6a618e5ced00be2719d995f
Duran <duran@gmail.com>
2022-08-11 11:08:21 +0200

    Updated Currency Converter service

--- grails-app/services/com/i3zone/demo/ConvertCurrencyService.groovy
+++ grails-app/services/com/i3zone/demo/ConvertCurrencyService.groovy
@@ -7,25 +7,36 @@ import groovy.json.JsonSlurper
 import io.micrometer.core.ipc.http.HttpSender
 import io.micronaut.http.client.netty.DefaultHttpClient
 import org.junit.runner.Request
-import org.springframework.http.HttpRequest
 import java.net.http.HttpClient
+import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.stream.Stream
 
 
 class ConvertCurrencyService{
 
     def convertZARToUSD(double zar) {
 
-        URL url = new URL("https://api.apilayer.com/fixer/latest?apikey=0I0UHTXSBTltwkcqo6pWv6l12z6mIuIV&symbols=USD,ZAR")
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://api.apilayer.com/fixer/convert?to=USD&from=ZAR&amount=${zar}"))
+                .headers("apikey", "0I0UHTXSBTltwkcqo6pWv6l12z6mIuIV")
+                .GET()
+                .build()
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))
+        HttpResponse<InputStream> response = HttpClient.newBuilder().build()
+                .send(request, HttpResponse.BodyHandlers.ofInputStream())
 
-        JsonSlurper slurper = new JsonSlurper()
 
-        Object result = slurper.parse(reader)
+        if( response.statusCode() == 200) {
 
-        reader.close()
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.body()))
 
-        return (zar / result.rates.ZAR) * result.rates.USD
+            JsonSlurper slurper = new JsonSlurper()
+
+            Object obj = slurper.parse(reader)
+
+            return obj.result
+        }
+
+        return 0.0
     }
 }
